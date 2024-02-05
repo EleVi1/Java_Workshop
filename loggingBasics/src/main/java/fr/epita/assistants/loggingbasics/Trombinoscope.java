02ebbf547e2506b5b67d31247c65ae097c6d4c9e
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-05 13:47:54 +0100

    first

---
+++ loggingBasics/src/main/java/fr/epita/assistants/loggingbasics/Trombinoscope.java
@@ -0,0 +1,33 @@
+package fr.epita.assistants.loggingbasics;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import org.apache.maven.plugin.logging.Log;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class Trombinoscope {
+    private final Logger logger;
+    private final HashMap<String, Long> map;
+
+    public Trombinoscope() {
+        logger = (Logger) LoggerFactory.getLogger(Trombinoscope.class);
+        logger.setLevel(Level.TRACE);
+
+        logger.trace("Instantiating new Trombinoscope");
+        map = new HashMap<>();
+    }
+
+    public Long putPerson(String name, long photoId) {
+        // FIXME: Add logging here
+        logger.debug("Putting person (\"%s\", %d)".formatted(name, photoId));
+        Long oldPhotoId = map.put(name, photoId);
+
+        logger.trace("Added entry for person \"%s\"".formatted(name));
+
+        // FIXME: Add logging here
+
+        return oldPhotoId;
+    }
+}
