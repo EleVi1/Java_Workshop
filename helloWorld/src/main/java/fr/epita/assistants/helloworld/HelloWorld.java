39dd29072ac1572df4c1aa61e370338ec05e2931
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-05 10:40:34 +0100

    hello world

---
+++ helloWorld/src/main/java/fr/epita/assistants/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package fr.epita.assistants.helloworld;
+
+public class HelloWorld {
+    public void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
+
+    public void printHelloWorldErr() {
+        System.err.println("Hello World!" + System.lineSeparator());
+    }
+}
+
