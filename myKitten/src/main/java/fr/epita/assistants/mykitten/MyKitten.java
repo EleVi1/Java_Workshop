c688f76aa29141ee2503b5f80c2681714d0e0a63
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-06 20:26:10 +0100

    first

---
+++ myKitten/src/main/java/fr/epita/assistants/mykitten/MyKitten.java
@@ -0,0 +1,66 @@
+package fr.epita.assistants.mykitten;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.io.IOException;
+
+public class MyKitten {
+    /**
+     * Initializer.
+     *
+     * @param srcPath Source file path.
+     */
+    String path;
+    Stream<String> stream_content;
+    public MyKitten(String srcPath) {
+        path = srcPath;
+        try {
+            streamContent = Files.lines(Paths.get(srcPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Use the streamContent to replace `wordToReplace` with "miaou". Don't forget
+     * to add the line number beforehand for each line. Store the new
+     * result directly in the streamContent field.
+     *
+     * @param wordToReplace The word to replace
+     */
+    public void replaceByMiaou(String wordToReplace) {
+        streamContent = streamContent.map(line -> line.replaceAll(wordToReplace, "miaou"));
+    }
+
+    /**
+     * Use the streamContent to write the content into the destination file.
+     *
+     * @param destPath Destination file path.
+     */
+    public void toFile(String destPath) {
+        try {
+            Files.write(Paths.get(destPath), streamContent.toList(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Creates an instance of MyKitten and calls the above methods to do it
+     * straightforwardly.
+     *
+     * @param srcPath       Source file path
+     * @param destPath      Destination file path
+     * @param wordToReplace Word to replace
+     */
+    public static void miaou(String srcPath, String destPath,
+                             String wordToReplace) {
+        MyKitten kitt = new MyKitten(srcPath);
+        kitt.replaceByMiaou(wordToReplace);
+        kitt.toFile(destPath);
+    }
+
+    public Stream<String> streamContent;
+}
