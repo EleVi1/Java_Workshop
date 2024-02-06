b7a754107ef0cd445481a5d7d71faa00ea83fbea
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-06 13:37:41 +0100

    first

--- singleton/src/main/java/fr/epita/assistants/singleton/SingletonEnumLogger.java
+++ singleton/src/main/java/fr/epita/assistants/singleton/SingletonEnumLogger.java
@@ -1,4 +1,54 @@
 package fr.epita.assistants.singleton;
 
-public class SingletonEnumLogger {
+import fr.epita.assistants.logger.Logger;
+
+import java.util.List;
+
+public enum SingletonEnumLogger implements Logger {
+
+    INSTANCE;
+    int info_counter = 0;
+    int error_counter = 0;
+    int warn_counter = 0;
+
+    @Override
+    public void log(Level level, String message) {
+        if (level == Level.INFO)
+        {
+            info_counter++;
+            System.err.println("[INFO] " + message);
+        }
+        if (level == Level.WARN)
+        {
+            warn_counter++;
+            System.err.println("[WARN] " + message);
+        }
+        if (level == Level.ERROR)
+        {
+            error_counter++;
+            System.err.println("[ERROR] " + message);
+        }
+    }
+
+    @Override
+    public int getInfoCounter() {
+        return info_counter;
+    }
+
+    @Override
+    public int getWarnCounter() {
+        return warn_counter;
+    }
+
+    @Override
+    public int getErrorCounter() {
+        return error_counter;
+    }
+
+    @Override
+    public void reset() {
+        info_counter = 0;
+        error_counter = 0;
+        warn_counter = 0;
+    }
 }
--- singleton/src/main/java/fr/epita/assistants/singleton/StaticSingletonLogger.java
+++ singleton/src/main/java/fr/epita/assistants/singleton/StaticSingletonLogger.java
@@ -1,4 +1,61 @@
 package fr.epita.assistants.singleton;
 
-public class StaticSingletonLogger {
+import fr.epita.assistants.logger.Logger;
+
+public class StaticSingletonLogger implements Logger {
+
+    int info_counter = 0;
+    int error_counter = 0;
+    int warn_counter = 0;
+    private StaticSingletonLogger() {
+    }
+    private static class InstanceHolder {
+        private static StaticSingletonLogger _INSTANCE;
+    }
+
+    static StaticSingletonLogger getInstance()
+    {
+        return InstanceHolder._INSTANCE;
+    }
+
+    @Override
+    public void log(Level level, String message) {
+        if (level == Level.INFO)
+        {
+            info_counter++;
+            System.err.println("[INFO] " + message);
+        }
+        if (level == Level.WARN)
+        {
+            warn_counter++;
+            System.err.println("[WARN] " + message);
+        }
+        if (level == Level.ERROR)
+        {
+            error_counter++;
+            System.err.println("[ERROR] " + message);
+        }
+    }
+
+    @Override
+    public int getInfoCounter() {
+        return info_counter;
+    }
+
+    @Override
+    public int getWarnCounter() {
+        return warn_counter;
+    }
+
+    @Override
+    public int getErrorCounter() {
+        return error_counter;
+    }
+
+    @Override
+    public void reset() {
+        info_counter = 0;
+        warn_counter = 0;
+        error_counter = 0;
+    }
 }
