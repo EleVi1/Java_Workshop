2d00eb1ffde1c5f7f5db1942e1599eb2d60a6a09
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-08 15:05:51 +0100

    first

---
+++ miniStreamsMusic/src/main/java/fr/epita/assistants/ministreamsmusic/dataaccess/StreamsSongs.java
@@ -0,0 +1,22 @@
+package fr.epita.assistants.ministreamsmusic.dataaccess;
+
+import fr.epita.assistants.ministreamsmusic.data.Song;
+
+import java.util.List;
+
+public class StreamsSongs {
+
+    public static List<String> getOlderArtists(List<Song> songs)
+    {
+        return songs.stream().filter(e -> e.getArtist().getAge() >= 30).map(e -> e.getArtist().getSurname()).limit(10).toList();
+    }
+    public static Integer getSumAges(List<Song> songs)
+    {
+        return songs.stream().filter(e -> e.getArtist().getAge() >= 20).mapToInt(e -> e.getArtist().getAge()).sum();
+    }
+
+    public static List<String> getMusics(List<Song> songs)
+    {
+        return songs.stream().filter(e -> e.getArtist().getName().matches("^.*(An|an|aN|AN).*$")).map(e -> e.getName()).limit(10).toList();
+    }
+}
